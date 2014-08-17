d8438868dd681a28131cf27f956b69c7763674ec
juljun14 <juljun14@gmail.com>
2014-08-17 17:13:21 -0400

    create a new array keys in order to add all keys into it

--- app/assets/javascripts/hotkeys.js
+++ app/assets/javascripts/hotkeys.js
@@ -3,4 +3,6 @@ function createHotkeys() {
   key2 = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
   key3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
   key4 = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
+
+  keys.push(key1, key2, key3, key4);
 }
